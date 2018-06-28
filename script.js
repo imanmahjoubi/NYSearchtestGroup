f55c337ce327335d41d58c137d2a65dd94ede4cd
Imen Mahjoubi <imen.mahjoubi@esprit.tn>
2018-06-27 21:22:09 -0700

    second  add

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-// Built by LucyBot. www.lucybot.com
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-    'api-key': "5873ddfa2262460fa3086ff5ef7449e6"
-});
-$.ajax({
-    url: url,
-    method: 'GET',
-}).done(function(result) {
-    console.log(result);
-}).fail(function(err) {
-    throw err;
+
+$(document).ready(function () {
+    // Built by LucyBot. www.lucybot.com
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+        'api-key': "5873ddfa2262460fa3086ff5ef7449e6"
+    });
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function(result) {
+        console.log(result);
+
+    }).fail(function(err) {
+        throw err;
+    });
+
+
 });
